454ca174e2160cb3dfa56e4f3036e5bc620c357d
tasneemkhaled <khaledtasneem402@gmail.com>
2025-11-04 22:43:59 +0200

    login ui done

--- lib/features/authentication/presentation/views/login_view.dart
+++ lib/features/authentication/presentation/views/login_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/core/utils/constants.dart';
+import 'package:fruit_app/features/authentication/presentation/views/widgets/custom_button.dart';
+import 'package:fruit_app/features/authentication/presentation/views/widgets/custom_text_form_field.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -6,8 +9,70 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                'Fruit Market',
+                style: TextStyle(
+                  color: pColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Login To Wikala',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            Align(alignment: Alignment.topLeft, child: Text('Phone Number *')),
+            SizedBox(height: 5),
+            CustomTextFormField(
+              hintText: 'Mobile Number',
+              keyboardType: TextInputType.number,
+            ),
+            Align(alignment: Alignment.topLeft, child: Text('Password *')),
+            SizedBox(height: 5),
+            CustomTextFormField(hintText: 'Password'),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.topRight,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Forget password ?',
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 300,
+              child: CustomButton(
+                text: 'Login',
+                buttonColor: pColor,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Don\'t have an account?'),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(decoration: TextDecoration.underline),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
