54e781619c736f52c7421dad1b1a30cf5454ab96
tasneemkhaled <khaledtasneem402@gmail.com>
2025-11-10 20:37:12 +0200

    payemnt widget done

--- lib/features/checkout/presentation/views/widgets/address_widget.dart
+++ lib/features/checkout/presentation/views/widgets/address_widget.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/core/utils/constants.dart';
 
 class AddressWidget extends StatelessWidget {
   const AddressWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [],);
+    return Column(
+      children: [
+        SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Select Delivery Address',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Colors.grey.shade400),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text('Add New Location'), Icon(Icons.add)],
+            ),
+          ),
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Colors.grey.shade300),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Icon(Icons.map),
+                    Text(
+                      'Address 1',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                        color: pColor,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.grey, width: 2),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.check, color: Colors.white, size: 16),
+                      ),
+                    ),
+                  ],
+                ),
+                Align(alignment: Alignment.topLeft, child: Text('John Doe')),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text('010113333344'),
+                ),
+                Text(
+                  'Room # 1 - Ground Floor, Al Najoum Building, 24 B Street, Dubai - United Arab Emirates',
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height / 3),
+        SizedBox(
+          width: MediaQuery.sizeOf(context).width * (3.5 / 4),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: pColor),
+            onPressed: () {},
+            child: Text('Continue', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/features/checkout/presentation/views/widgets/payment_widget.dart
+++ lib/features/checkout/presentation/views/widgets/payment_widget.dart
@@ -1,10 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/core/utils/constants.dart';
+import 'package:fruit_app/features/checkout/presentation/views/widgets/way_of_payement.dart';
 
 class PaymentWidget extends StatelessWidget {
   const PaymentWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: []);
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Coupon Code',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        Stack(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Do You Have Any Coupon Code?',
+                hintStyle: TextStyle(fontSize: 15),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 8,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: pColor),
+                onPressed: () {},
+                child: Text('Apply', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Order Details',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Total Items'),
+            Text(
+              '4 Items in cart',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Subtotal'),
+            Text(
+              '36.00 KD',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Shipping Charges'),
+            Text(
+              '1.50 KD',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ],
+        ),
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Bag Total',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            Text(
+              '37.50 KD',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ],
+        ),
+        SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Payement',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        WayOfPayement(),
+        // SizedBox(height: MediaQuery.sizeOf(context).height / 3),
+        SizedBox(
+          width: MediaQuery.sizeOf(context).width * (3.5 / 4),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: pColor),
+            onPressed: () {},
+            child: Text('Place Order', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ],
+    );
   }
 }
